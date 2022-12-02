4d5357b1a87d0594acefa9618bcc5cbae72c1c10
AndyBao <junwei.bao@visionacademy.cn>
2022-12-02 15:59:59 +0800

    learning leyou

---
+++ learning-leyou/learning-leyou-manage/src/views/Login.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-app class="split-bg">
+    <v-content>
+      <v-container fill-height fluid>
+        <v-layout align-center justify-center>
+          <v-flex lg4 md4 sm8 xs12>
+            <v-card class="elevation-1 pa-3">
+              <v-card-text>
+                <div class="layout column align-center">
+
+                  <h1 class="flex my-4 primary--text">
+                    乐优商城后台管理
+                  </h1>
+                </div>
+                <v-form>
+                  <v-text-field
+                      v-model="model.username"
+                      append-icon="person"
+                      label="Login"
+                      name="login"
+                      type="text"
+                      @keyup.enter="login"
+                  ></v-text-field>
+                  <v-text-field
+                      id="password"
+                      v-model="model.password"
+                      append-icon="lock"
+                      label="Password"
+                      name="password"
+                      type="password"
+                      @keyup.enter="login"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <div class="login-btn">
+                <v-btn icon>
+                  <v-icon color="blue">fa fa-facebook-square fa-lg</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon color="red">fa fa-google fa-lg</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon color="light-blue">fa fa-twitter fa-lg</v-icon>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn :loading="loading" block color="primary" @click="login">Login</v-btn>
+              </div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    model: {
+      username: "admin@itcast.cn",
+      password: "admin"
+    }
+  }),
+  methods: {
+    login() {
+      if (!this.model.username || !this.model.password) {
+        this.loading = false;
+        return false;
+      }
+
+      // 模拟登录
+      this.loading = true;
+      setTimeout(() => this.$router.push("/index/dashboard"), 400);
+      /*this.$http.post("/auth/login", this.model)
+        .then(resp => {
+          this.loading = true;
+          this.$router.push("/index/dashboard");
+        })*/
+    }
+  }
+};
+</script>
+<style lang="css" scoped>
+.split-bg {
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+  z-index: 0;
+  background-color: #3F51B5;
+}
+</style>
---
+++ learning-leyou/learning-leyou-manage/src/views/item/goods/GoodsForm.vue
@@ -0,0 +1,539 @@
+﻿<template>
+  <v-stepper v-model="step" class="elevation-0">
+    <v-stepper-header>
+      <v-stepper-step :complete="step > 1" step="1">基本信息</v-stepper-step>
+      <v-divider/>
+      <v-stepper-step :complete="step > 2" step="2">商品描述</v-stepper-step>
+      <v-divider/>
+      <v-stepper-step :complete="step > 3" step="3">规格参数</v-stepper-step>
+      <v-divider/>
+      <v-stepper-step step="4">SKU属性</v-stepper-step>
+    </v-stepper-header>
+    <v-stepper-items>
+      <!--1、基本信息-->
+      <v-stepper-content step="1">
+        <v-flex class="xs10 mx-auto">
+          <v-form ref="basic" v-model="valid">
+            <!--商品分类-->
+            <v-cascader
+                v-model="goods.categories"
+                label="请选择商品分类"
+                required
+                showAllLevels
+                url="/item/category/of/parent"/>
+            <!--品牌-->
+            <v-autocomplete
+                v-model="goods.brandId"
+                :items="brandOptions"
+                :rules="[v => !!v || '品牌不能为空']"
+                autocomplete
+                chips
+                clearable
+                dense
+                item-text="name"
+                item-value="id" label="所属品牌"
+                required
+            >
+              <template slot="selection" slot-scope="data">
+                <v-chip small>{{ data.item.name }}</v-chip>
+              </template>
+            </v-autocomplete>
+            <v-text-field v-model="goods.name" :counter="200" :rules="[v => !!v || '商品标题不能为空']" label="商品名称"
+                          required/>
+            <v-textarea v-model="goods.title" :counter="200" :rows="4" label="商品标题(会影响到搜索排名)"/>
+          </v-form>
+        </v-flex>
+      </v-stepper-content>
+      <!--2、商品描述-->
+      <v-stepper-content step="2">
+        <v-textarea v-model="goods.spuDetail.packingList" :counter="1000" :rows="3" hide-details label="包装清单"/>
+        <v-textarea v-model="goods.spuDetail.afterService" :counter="1000" :rows="3" class="mb-4" hide-details
+                    label="售后服务"/>
+        <span class="subheading font-weight-thin">商品描述</span>
+        <v-editor v-model="goods.spuDetail.description" class="mt-3" needSignature url="/auth/ali/oss/signature"/>
+      </v-stepper-content>
+      <!--3、规格参数-->
+      <v-stepper-content step="3">
+        <v-card>
+          <v-flex class="headline mx-auto" xs3>通用规格属性</v-flex>
+          <v-card-text>
+            <v-container class="mt-0" fluid grid-list-md>
+
+              <v-layout class="px-4" justify-space-between row wrap>
+                <v-flex v-for="param in specs" :key="param.name" sm5 xs12>
+                  <v-select v-if="param.ops && param.ops.length > 0" v-model="param.v" :items="param.ops"
+                            :label="param.name"></v-select>
+                  <v-text-field v-else v-model="param.v" :label="param.name" :suffix="param.unit || ''"
+                  />
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+        </v-card>
+        <!--遍历特有规格参数-->
+        <v-divider></v-divider>
+        <v-card>
+          <v-flex class="headline mx-auto" xs3>特有用规格属性</v-flex>
+          <v-card-text>
+            <v-card v-for="spec in specialSpecs" :key="spec.name" flat>
+              <!--特有参数的标题-->
+              <v-flex class="subheading">{{ spec.name }}:</v-flex>
+              <!--特有参数的待选项，需要判断是否有options，如果没有，展示文本框，让用户自己输入-->
+              <v-flex>
+                <v-card-text class="px-4">
+                  <v-container v-if="spec.ops && spec.ops.length > 0" class="mt-0" fluid grid-list-md>
+                    <v-layout class="px-4" justify-space-between row wrap>
+                      <v-flex v-for="(o,i) in spec.ops" :key="o" sm3 xs12>
+                        <v-checkbox v-model="spec.options" :label="o" :value="o" color="primary" hide-details/>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+
+                  <v-container v-else class="mt-0" fluid grid-list-md>
+                    <v-layout class="px-4" justify-space-between row wrap>
+                      <v-flex v-for="i in spec.options.length+1" :key="i" class="row layout" sm6 xs12>
+                        <v-text-field v-model="spec.options[i-1]" :placeholder="'新的' + spec.name + ':'" auto-grow
+                                      class="flex xs10" hide-details single-line v-bind:value="i"/>
+                        <v-btn v-if="i <= spec.options.length" icon @click="spec.options.splice(i-1,1)">
+                          <i class="el-icon-delete"/>
+                        </v-btn>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                  <v-divider></v-divider>
+                </v-card-text>
+              </v-flex>
+            </v-card>
+          </v-card-text>
+        </v-card>
+
+
+      </v-stepper-content>
+      <!--4、SKU属性-->
+      <v-stepper-content step="4">
+        <v-flex class="mx-auto">
+
+          <v-card class="elevation-0">
+            <!--标题-->
+            <div class="subheading py-3">SKU列表:</div>
+            <v-divider/>
+            <!--SKU表格，hide-actions因此分页等工具条-->
+            <v-data-table :headers="headers" :items="skus" class="elevation-0" hide-actions item-key="indexes">
+              <template slot="items" slot-scope="props">
+                <tr @click="props.expanded = !props.expanded">
+                  <!--价格和库存展示为文本框-->
+                  <td v-for="(v,k) in props.item" v-if="['price', 'stock'].includes(k)" :key="k"
+                      class="text-xs-center">
+                    <v-text-field v-model="props.item[k]" single-line @click.stop=""/>
+                  </td>
+                  <!--enable展示为checkbox-->
+                  <td v-else-if="k === 'enable'" class="text-xs-center">
+                    <v-checkbox v-model="props.item[k]"/>
+                  </td>
+                  <!--indexes和images不展示，其它展示为普通文本-->
+                  <td v-else-if="k !== 'images' && k !== 'indexes'&& k !== 'id'" class="text-xs-center">{{ v.v }}</td>
+                </tr>
+              </template>
+              <!--点击表格后展开-->
+              <template slot="expand" slot-scope="props">
+                <v-card class="elevation-2 flex xs11 mx-auto my-2">
+                  <!--图片上传组件-->
+                  <v-upload v-model="props.item.images" needSignature url="/auth/ali/oss/signature"/>
+                </v-card>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-flex>
+        <!--提交按钮-->
+        <v-flex offset-xs9 xs3>
+          <v-btn color="info" @click="submit">保存商品信息</v-btn>
+        </v-flex>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  name: "goods-form",
+  props: {
+    oldGoods: {
+      type: Object
+    },
+    isEdit: {
+      type: Boolean,
+      default: false
+    },
+    step: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      valid: false,
+      goods: {
+        categories: [], // 商品分类信息
+        brandId: 0, // 品牌id信息
+        name: "", // 标题
+        title: "", // 子标题
+        spuDetail: {
+          packingList: "", // 包装列表
+          afterService: "", // 售后服务
+          description: "" // 商品描述
+        }
+      },
+      brandOptions: [], // 品牌列表
+      specs: [], // 规格参数的模板
+      specialSpecs: [], // 特有规格参数模板
+    };
+  },
+  methods: {
+    submit() {
+      if (this.isEdit) {
+        this.handleEdit();
+      } else {
+        this.handleAdd();
+      }
+    },
+    handleAdd() {
+      // 表单校验。
+      if (!this.$refs.basic.validate) {
+        this.$message.error("请先完成表单内容！");
+      }
+      // 先处理goods，用结构表达式接收,除了categories外，都接收到goodsParams中
+      const {
+        categories: [{id: cid1}, {id: cid2}, {id: cid3}],
+        ...goodsParams
+      } = this.goods;
+      // 处理规格参数
+      const specs = {};
+      this.specs.forEach(({id, v}) => {
+        specs[id] = v;
+      });
+      this.specialSpecs.forEach(({id, options}) => {
+        specs[id] = options;
+      });
+      // 处理sku
+      const skus = this.skus
+          .filter(s => s.enable)
+          .map(({price, stock, enable, images, indexes, ...rest}) => {
+            // 标题，在spu的name基础上，拼接特有规格属性值
+            const title = goodsParams.name + " " + Object.values(rest).map(v => v.v).join(" ");
+            const obj = {};
+            Object.values(rest).forEach(v => {
+              obj[v.id] = v.v;
+            });
+            return {
+              price: this.$format(price), // 价格需要格式化
+              stock,
+              indexes,
+              title, // 基本属性
+              images: images ? images.join(",") : '', // 图片
+              specialSpec: JSON.stringify(obj) // 特有规格参数
+            };
+          });
+      Object.assign(goodsParams, {
+        cid1,
+        cid2,
+        cid3, // 商品分类
+        skus // sku列表
+      });
+      goodsParams.spuDetail.specification = JSON.stringify(specs);
+      this.$http({
+        method: this.isEdit ? "put" : "post",
+        url: "/item/goods/spu",
+        data: goodsParams
+      })
+          .then(() => {
+            // 成功，关闭窗口
+            this.$emit("close");
+            // 提示成功
+            this.$message.success("保存成功了");
+          })
+          .catch(() => {
+            this.$message.error("保存失败！");
+          });
+    },
+    handleEdit() {
+      // 1.判断spu是否修改
+      // 1.1.获取spu
+      let {id, categories, brandId, name, title} = this.goods;
+      let spu = {id, brandId, name, title, cid1: categories[0].id, cid2: categories[1].id, cid3: categories[2].id};
+      let spuTitle = title;
+      // 1.2.判断spu是否有修改
+      const isSpuChange = !Object.compareObj(spu, this.oldGoods);
+      if (!isSpuChange) {
+        spu = {};
+      }
+      // 2.判断spuDetail是否修改
+
+      let {...detail} = this.goods.spuDetail;
+      const specs = {};
+      this.specs.forEach(({id, v}) => {
+        specs[id] = v;
+      });
+      this.specialSpecs.forEach(({id, options}) => {
+        specs[id] = options;
+      });
+      detail.specification = JSON.stringify(specs);
+      // 2.3.判断spuDetail是否有修改
+      let isDetailChange = false;
+      for (let k in detail) {
+        if (detail[k] !== this.oldGoods.spuDetail[k]) {
+          isDetailChange = true;
+        } else {
+          delete detail[k];
+        }
+      }
+      if (isDetailChange) {
+        detail.spuId = this.goods.id;
+
+        spu.spuDetail = detail;
+      }
+      // 3.判断sku是否修改
+      // 3.1.处理sku
+      const skus = this.skus
+          .map(({title, price, stock, enable, images, indexes, id, ...rest}) => {
+            if (!enable) {
+              return {id, enable: false}
+            }
+            const obj = {};
+            Object.values(rest).forEach(v => {
+              obj[v.id] = v.v;
+            });
+            return {
+              id,
+              title: title || spuTitle + " " + Object.values(obj).join(" "),
+              price: this.$format(price), // 价格需要格式化
+              stock,
+              indexes,
+              images: images ? images.join(",") : '', // 图片
+              specialSpec: JSON.stringify(obj) // 特有规格参数
+            };
+          });
+      // 3.3.判断sku是否修改
+      let isSkuChange = false;
+      for (let i = 0; i < skus.length; i++) {
+        let s = skus[i];
+        let os = this.oldGoods.skus.find(v => v.id === s.id);
+        if (!Object.compareObj(s, os)) {
+          isSkuChange = true;
+          if (!os && s.enable === false) {
+            skus.splice(i, 1);
+            i--;
+          } else if (os && s.enable === false) {
+            s.saleable = false;
+          }
+        } else {
+          skus.splice(i, 1);
+          i--;
+        }
+        s.spuId = id;
+      }
+      if (isSkuChange) {
+        spu.skus = skus;
+      }
+      if (isSpuChange || isSkuChange || isDetailChange) {
+        // 4.发起请求
+        this.$http.put("/item/goods/spu", spu)
+            .then(() => {
+              // 成功，关闭窗口
+              this.$emit("close");
+              // 提示成功
+              this.$message.success("保存成功了");
+            })
+            .catch(() => {
+              this.$message.error("保存失败！");
+            });
+      } else {
+        // 成功，关闭窗口
+        this.$emit("close");
+        // 提示成功
+        this.$message.warning("商品没有修改！");
+      }
+    }
+  },
+  watch: {
+    oldGoods: {
+      deep: true,
+      handler(val) {
+        if (!this.isEdit) {
+          Object.assign(this.goods, {
+            categories: null, // 商品分类信息
+            brandId: 0, // 品牌id信息
+            name: "", // 标题
+            title: "", // 子标题
+            spuDetail: {
+              packingList: "", // 包装列表
+              afterService: "", // 售后服务
+              description: "" // 商品描述
+            }
+          });
+          this.specs = [];
+          this.specialSpecs = [];
+        } else {
+          this.goods = Object.deepCopy(val);
+
+          // 先得到分类名称
+          const names = val.categoryName.split("/");
+          delete this.goods.categoryName;
+          delete this.goods.brandName;
+          delete this.goods.createTime;
+          delete this.goods.saleable;
+          // 组织商品分类数据
+          this.goods.categories = [
+            {id: val.cid1, name: names[0]},
+            {id: val.cid2, name: names[1]},
+            {id: val.cid3, name: names[2]}
+          ];
+
+          // 将skus处理成map
+          const skuMap = new Map();
+          this.goods.skus.forEach(s => {
+            s.enable = true;
+            skuMap.set(s.indexes, s);
+          });
+          this.goods.skus = skuMap;
+        }
+      }
+    },
+    "goods.categories": {
+      deep: true,
+      handler(val) {
+        // 判断商品分类是否存在，存在才查询
+        if (val && val.length > 0) {
+          // 根据分类查询品牌
+          this.$http
+              .get("/item/brand/of/category?id=" + this.goods.categories[2].id)
+              .then(({data}) => {
+                this.brandOptions = data;
+              });
+          // 根据分类查询规格参数
+          this.$http
+              .get("/item/spec/params?categoryId=" + this.goods.categories[2].id)
+              .then(({data}) => {
+                let specs = [];
+                let template = [];
+                if (this.isEdit) {
+                  specs = JSON.parse(this.goods.spuDetail.specification);
+                }
+                // 对特有规格进行筛选
+                const arr1 = [];
+                const arr2 = [];
+                data.forEach(({id, name, generic, numeric, unit, options}) => {
+                  if (generic) {
+                    const o = {id, name, numeric, unit, ops: options ? options.split(',') : []};
+                    if (this.isEdit) {
+                      o.v = specs[id];
+                    }
+                    arr1.push(o)
+                  } else {
+                    const o = {id, name, options: [], ops: options ? options.split(',') : []};
+                    if (this.isEdit) {
+                      o.options = specs[id];
+                    }
+                    arr2.push(o)
+                  }
+                });
+                this.specs = arr1;// 通用规格
+                this.specialSpecs = arr2;// 特有规格
+              });
+        }
+      }
+    },
+    goods: {
+      deep: true,
+      handler() {
+        if (this.isEdit) {
+          this.modified = true;
+        }
+      }
+    }
+  },
+  computed: {
+    skus() {
+      // 过滤掉用户没有填写数据的规格参数
+      const arr = this.specialSpecs.filter(s => s.options.length > 0);
+      // 通过reduce进行累加笛卡尔积
+      return arr.reduce(
+          (last, spec, index) => {
+            const result = [];
+            last.forEach(o => {
+              spec.options.forEach((option, i) => {
+                const obj = JSON.parse(JSON.stringify(o));
+                obj[spec.name] = {v: option, id: spec.id};
+                obj.indexes = (obj.indexes || '') + '_' + i
+                if (index === arr.length - 1) {
+                  obj.indexes = obj.indexes.substring(1);
+                  // 如果发现是最后一组，则添加价格、库存等字段
+                  Object.assign(obj, {
+                    price: 0,
+                    stock: 0,
+                    enable: false,
+                    images: []
+                  });
+                  if (this.isEdit) {
+                    // 如果是编辑，则回填sku信息
+                    const sku = this.goods.skus.get(obj.indexes);
+                    if (sku != null) {
+                      const {title, price, stock, enable, images, id} = sku;
+                      Object.assign(obj, {
+                        id,
+                        price: this.$format(price),
+                        title,
+                        stock,
+                        enable,
+                        images: images ? images.split(",") : [],
+                      });
+                    }
+                  }
+                }
+                result.push(obj);
+              });
+            });
+            return result;
+          },
+          [{}]
+      );
+    },
+    headers() {
+      if (this.skus.length <= 0) {
+        return [];
+      }
+      const headers = [];
+      Object.keys(this.skus[0]).forEach(k => {
+        let value = k;
+        let width = "";
+        if (k === "price") {
+          // enable，表头要翻译成“价格”
+          k = "价格";
+          width = "120px";
+        } else if (k === "stock") {
+          // enable，表头要翻译成“库存”
+          width = "120px";
+          k = "库存";
+        } else if (k === "enable") {
+          // enable，表头要翻译成“是否启用”
+          width = "80px";
+          k = "是否启用";
+        } else if (k === "images" || k === 'indexes' || k === 'id' || k === 'title') {
+          // 图片和索引不在表格中展示
+          return;
+        }
+        headers.push({
+          text: k,
+          align: "center",
+          sortable: false,
+          value,
+          width
+        });
+      });
+      return headers;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
