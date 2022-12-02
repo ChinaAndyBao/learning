f0ec6bede7b8dc3a9ec77cc04fdd38c0a27109c5
AndyBao <junwei.bao@visionacademy.cn>
2022-12-02 16:59:22 +0800

    learning leyou

---
+++ learning-leyou/learning-leyou-gateway/src/main/java/cn/andybao/LyGateway.java
@@ -0,0 +1,17 @@
+package cn.andybao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author ${USER}
+ * @description
+ * @create_at ${DATE} ${TIME}
+ * @since
+ */
+@SpringCloudApplication
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class, args);
+    }
+}
---
+++ learning-leyou/learning-leyou-registry/src/main/java/cn/andybao/LyRegistry.java
@@ -0,0 +1,19 @@
+package cn.andybao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author AndyBao
+ * @description
+ * @create_at 2022/11/25 17:13
+ * @since
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class, args);
+    }
+}
