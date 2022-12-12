acd1c0a90061222a6139e42cc9b6e98d8bd64e1e
AndyBao <junwei.bao@visionacademy.cn>
2022-12-12 10:30:42 +0800

    learning mq

---
+++ learning-mq/learning-mq-rocket/src/main/java/cn/andybao/RocketMQ.java
@@ -0,0 +1,13 @@
+package cn.andybao;
+
+/**
+ * @author ${USER}
+ * @description
+ * @create_at ${DATE} ${TIME}
+ * @since
+ */
+public class RocketMQ {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
